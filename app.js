f6fb570ba547583031d8fddd141bd71a8fc4d601
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-07-14 10:45:18 -0400

    Practice Complete

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ require("./config")(app);
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
+const projectRouter = require("./routes/project.routes");     // <== IMPORT
+app.use("/api", projectRouter);  
+
+const taskRouter = require("./routes/task.routes");    // <== IMPORT
+app.use("/api", taskRouter);                        // <== ADD
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
